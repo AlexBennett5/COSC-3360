e57f3e3093969d67e90f4d43e2ed629ae40897c7
AlexBennett5 <bnet505@gmail.com>
2020-10-20 16:39:35 -0500

    Finishing touches on assignment 2

--- Assignment2/client.cpp
+++ Assignment2/client.cpp
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <string>
+#include <strings.h>
 #include <vector>
 #include <pthread.h>
 #include <unistd.h>
@@ -12,161 +14,167 @@
 #define MAXTHREAD 100
 
 void error(char *msg) {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
 }
 
 typedef struct Address {
-	unsigned int val[4];
-
+  unsigned int val[4];
 } Address;
 
 typedef struct AddressObj {
-
-	Address ip;
-	Address subnet;
-	Address network;
-	Address broadcast;
-	Address minHost;
-	Address maxHost;
-	int hosts;
-
+  Address ip;
+  Address subnet;
+  Address network;
+  Address broadcast;
+  Address minHost;
+  Address maxHost;
+  int hosts;
 } AddressObj;
 
 typedef struct ServerObj {
-	int portno;
-	char* servername;
-	AddressObj adrObj;
+  int portno;
+  char* servername;
+  AddressObj adrObj;
 } ServerObj;
 
 void printAddress(Address adr) {
-	printf("%d.%d.%d.%d\n", adr.val[0], adr.val[1], adr.val[2], adr.val[3]);
+  printf("%d.%d.%d.%d\n", adr.val[0], adr.val[1], adr.val[2], adr.val[3]);
 }
 
 void printInfo(AddressObj adr) {
-	printf("IP Address: ");
-	printAddress(adr.ip);
-	printf("Subnet: ");
-	printAddress(adr.subnet);
-	printf("Network: ");
-	printAddress(adr.network);
-	printf("Broadcast: ");
-	printAddress(adr.broadcast);
-	printf("HostMin: ");
-	printAddress(adr.minHost);
-	printf("HostMax: ");
-	printAddress(adr.maxHost);
-	printf("# Hosts: %d\n\n", adr.hosts);
+  printf("IP Address: ");
+  printAddress(adr.ip);
+  printf("Subnet: ");
+  printAddress(adr.subnet);
+  printf("Network: ");
+  printAddress(adr.network);
+  printf("Broadcast: ");
+  printAddress(adr.broadcast);
+  printf("HostMin: ");
+  printAddress(adr.minHost);
+  printf("HostMax: ");
+  printAddress(adr.maxHost);
+  printf("# Hosts: %d\n\n", adr.hosts);
 }
 
 Address vectToAddress(std::vector<std::string> vect) {
-	Address ret;
-	ret.val[0] = std::stoi(vect[0]);
-	ret.val[1] = std::stoi(vect[1]);
-	ret.val[2] = std::stoi(vect[2]);
-	ret.val[3] = std::stoi(vect[3]);
-	return ret;
+  Address ret;
+  ret.val[0] = std::stoi(vect[0]);
+  ret.val[1] = std::stoi(vect[1]);
+  ret.val[2] = std::stoi(vect[2]);
+  ret.val[3] = std::stoi(vect[3]);
+  return ret;
 }
 
 void parseLine(std::string const &str, const char split, std::vector<std::string> &tokens) {
-	size_t start;
-	size_t end = 0;
+  size_t start;
+  size_t end = 0;
 
-	while ((start = str.find_first_not_of(split, end)) != std::string::npos) {
-		end = str.find(split, start);
-		tokens.push_back(str.substr(start, end - start));
-	}
+  while ((start = str.find_first_not_of(split, end)) != std::string::npos) {
+    end = str.find(split, start);
+    tokens.push_back(str.substr(start, end - start));
+  }
 }
 
 void* connectToServer(void *servObj_void_ptr) {
 
-	ServerObj *servObj = (ServerObj *) servObj_void_ptr;
+  ServerObj *servObj = (ServerObj *) servObj_void_ptr;
 
-	int sockfd, n;
-    	struct sockaddr_in serv_addr;
+  int sockfd, n;
+  struct sockaddr_in serv_addr;
     
-	int portno = servObj->portno;
-	struct hostent* server = gethostbyname(servObj->servername);
-	if (server == NULL) {
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
-	}
+  int portno = servObj->portno;
+  struct hostent* server = gethostbyname(servObj->servername);
+  if (server == NULL) {
+    fprintf(stderr, "ERROR, no such host\n");
+    exit(0);
+  }
 
-    	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    	if (sockfd < 0)
-        	error("ERROR opening socket");
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    char msg_open[] = "ERROR opening socket";
+    error(msg_open);
+  }
 
-    	bzero((char *) &serv_addr, sizeof(serv_addr));
-    	serv_addr.sin_family = AF_INET;
-   	bcopy((char *)server->h_addr,
-         	(char *)&serv_addr.sin_addr.s_addr,
-         	server->h_length);
-    	serv_addr.sin_port = htons(portno);
-    	if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
-        	error("ERROR connecting");
-    
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+  serv_addr.sin_port = htons(portno);
+      
+  if (connect(sockfd,(struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    char msg_connect[] = "ERROR connecting";
+    error(msg_connect);
+  }  
    
-    	n = write(sockfd, &(servObj->adrObj), sizeof(AddressObj));
-    	if (n < 0)
-        	 error("ERROR writing to socket");
-    
-    	n = read(sockfd, &(servObj->adrObj), sizeof(AddressObj));
-    	if (n < 0)
-         	error("ERROR reading from socket");
-    	close(sockfd);
-	return NULL;
+  n = write(sockfd, &(servObj->adrObj), sizeof(AddressObj));
+      
+  if (n < 0) {
+    char msg_write[] = "ERROR writing to socket";
+    error(msg_write);
+  }
+
+  n = read(sockfd, &(servObj->adrObj), sizeof(AddressObj));
+      
+  if (n < 0) {
+    char msg_read[] = "ERROR reading from socket";
+    error(msg_read);
+  }
+  
+  close(sockfd);
+  return NULL;
 }
 
 int ipCalculator(ServerObj res[], int portno, char* servername) {
-	pthread_t tid[MAXTHREAD];
-	std::string line;
-	int i = 0;
+  pthread_t tid[MAXTHREAD];
+  std::string line;
+  int i = 0;
 
-	while (std::cin >> line) {
-		std::vector<std::string> ip;
-		std::vector<std::string> subnet;
+  while (std::cin >> line) {
+    std::vector<std::string> ip;
+    std::vector<std::string> subnet;
 
-		parseLine(line, '.', ip);
-		std::cin >> line;
-		parseLine(line, '.', subnet);	
+    parseLine(line, '.', ip);
+    std::cin >> line;
+    parseLine(line, '.', subnet);  
 
-		res[i].adrObj.ip = vectToAddress(ip);
-		res[i].adrObj.subnet = vectToAddress(subnet);
-		res[i].portno = portno;
-		res[i].servername = servername;
+    res[i].adrObj.ip = vectToAddress(ip);
+    res[i].adrObj.subnet = vectToAddress(subnet);
+    res[i].portno = portno;
+    res[i].servername = servername;
 
-		if (pthread_create(&tid[i], NULL, connectToServer, &res[i])) {
-			fprintf(stderr, "Error creating thread\n");
-			return 1;
-		}
+    if (pthread_create(&tid[i], NULL, connectToServer, &res[i])) {
+      fprintf(stderr, "Error creating thread\n");
+      return 1;
+    }
 
-		++i;
-	
-	}
+    ++i;
+  
+  }
 
-	for (int k = 0; k < i; k++)
-		pthread_join(tid[k], NULL);
+  for (int k = 0; k < i; k++)
+    pthread_join(tid[k], NULL);
 
-	return i;
+  return i;
 }
 
 int main(int argc, char *argv[]) {
 
-	if (argc < 3) {
-          fprintf(stderr,"usage %s hostname port\n", argv[0]);
-          exit(0);
-        }
+  if (argc < 3) {
+    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    exit(0);
+  }
         
-	int portno = atoi(argv[2]);
-	char* servername = argv[1];
+  int portno = atoi(argv[2]);
+  char* servername = argv[1];
 
-	static ServerObj res[MAXTHREAD];
-	int n = ipCalculator(res, portno, servername);
+  static ServerObj res[MAXTHREAD];
+  int n = ipCalculator(res, portno, servername);
 
-	printf("\n");
-	for (int i = 0; i < n; i++) {
-		printInfo(res[i].adrObj);
-	}
+  printf("\n");
+  for (int i = 0; i < n; i++) {
+    printInfo(res[i].adrObj);
+  }
 
 }
 
--- Assignment2/server.cpp
+++ Assignment2/server.cpp
@@ -6,165 +6,185 @@
 #include <stdio.h>
 #include <sys/types.h> 
 #include <sys/socket.h>
+#include <sys/wait.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <signal.h>
 
-void error(char *msg)
-{
-    perror(msg);
-    exit(1);
+void error(char *msg) {
+  perror(msg);
+  exit(1);
 }
 
 typedef struct Address {
-	unsigned int val[4];
-
+  unsigned int val[4];
 } Address;
 
 typedef struct AddressObj {
-	
-	Address ip;
-	Address subnet;
-	Address network;
-	Address broadcast;
-	Address minHost;
-	Address maxHost;
-	int hosts;
-
+  Address ip;
+  Address subnet;
+  Address network;
+  Address broadcast;
+  Address minHost;
+  Address maxHost;
+  int hosts;
 } AddressObj;
 
 typedef struct Octet {
-	int val[6];
-
+  int val[6];
 } Octet;
 
 int numberOfZeroBits(int num) {
-        int count = 0;
-        for (int i = 0; i < 8; i++) {
-                if (!(num & 1))
-                        ++count;
-                num >>= 1;
-        }
-        return count;
+  int count = 0;
+  for (int i = 0; i < 8; i++) {
+    if (!(num & 1))
+      ++count;
+    num >>= 1;
+  }
+  return count;
 }
 
 int power(int base, int exp) {
-        int res = 1;
-        while (exp > 0) {
-                res *= base;
-                --exp;
-        }
-        return res;
+  int res = 1;
+  while (exp > 0) {
+    res *= base;
+    --exp;
+  }
+  return res;
 }
 
 int numberOfHosts(Address subnet) {
-        int count = 0;
-        for (int i = 0; i < 4; i++)
-                count += numberOfZeroBits(subnet.val[i]);
-        return power(2, count) - 2;
+  int count = 0;
+  for (int i = 0; i < 4; i++)
+    count += numberOfZeroBits(subnet.val[i]);
+  return power(2, count) - 2;
 }
 
 void octetTail(Octet* oct) {
-	oct->val[2] = oct->val[0] & oct->val[1];
-	oct->val[3] = oct->val[0] | (~oct->val[1] & 255);
-	oct->val[4] = oct->val[0] & oct->val[1];
-	oct->val[4]++;
-	oct->val[5] = oct->val[0] | (~oct->val[1] & 255);
-	oct->val[5]--;
+  oct->val[2] = oct->val[0] & oct->val[1];
+  oct->val[3] = oct->val[0] | (~oct->val[1] & 255);
+  oct->val[4] = oct->val[0] & oct->val[1];
+  oct->val[4]++;
+  oct->val[5] = oct->val[0] | (~oct->val[1] & 255);
+  oct->val[5]--;
 }
 
 void octetNormal(Octet* oct) {
-	oct->val[2] = oct->val[0] & oct->val[1];
-	oct->val[3] = oct->val[0] | (~oct->val[1] & 255);
-	oct->val[4] = oct->val[0] & oct->val[1];
-	oct->val[5] = oct->val[0] | (~oct->val[1] & 255);
+  oct->val[2] = oct->val[0] & oct->val[1];
+  oct->val[3] = oct->val[0] | (~oct->val[1] & 255);
+  oct->val[4] = oct->val[0] & oct->val[1];
+  oct->val[5] = oct->val[0] | (~oct->val[1] & 255);
 }
 
 
 void addressCalculator(AddressObj* adrObj) {
-	Octet oct[4];
+  Octet oct[4];
 
-	for (int i = 0; i < 4; i++) {
-		oct[i].val[0] = adrObj->ip.val[i];
-		oct[i].val[1] = adrObj->subnet.val[i];
-		if (i != 3) {
-			octetNormal(&oct[i]);
-		} else {
-			octetTail(&oct[i]);
-		}
-	}
+  for (int i = 0; i < 4; i++) {
+    oct[i].val[0] = adrObj->ip.val[i];
+    oct[i].val[1] = adrObj->subnet.val[i];
+    if (i != 3) {
+      octetNormal(&oct[i]);
+    } else {
+      octetTail(&oct[i]);
+    }
+  }
 
-	for (int i = 0; i < 4; i++) {
-		adrObj->network.val[i] = oct[i].val[2];
-		adrObj->broadcast.val[i] = oct[i].val[3];
-		adrObj->minHost.val[i] = oct[i].val[4];
-		adrObj->maxHost.val[i] = oct[i].val[5];
-	}
+  for (int i = 0; i < 4; i++) {
+    adrObj->network.val[i] = oct[i].val[2];
+    adrObj->broadcast.val[i] = oct[i].val[3];
+    adrObj->minHost.val[i] = oct[i].val[4];
+    adrObj->maxHost.val[i] = oct[i].val[5];
+  }
 
-	adrObj->hosts = numberOfHosts(adrObj->subnet);
+  adrObj->hosts = numberOfHosts(adrObj->subnet);
 }
 
 void processConnection(int sock) {
 
-       int n;
-       AddressObj adrObjIn;
+  int n;
+  AddressObj adrObjIn;
 
-       n = read(sock, &adrObjIn, sizeof(AddressObj));
-       if (n < 0) error("ERROR reading from socket");
+  n = read(sock, &adrObjIn, sizeof(AddressObj));
+       
+  if (n < 0) {
+    char msg_read[] = "ERROR reading from socket"; 
+    error(msg_read);
+  }
 
-       addressCalculator(&adrObjIn);
+  addressCalculator(&adrObjIn);
 
-       n = write(sock, &adrObjIn, sizeof(AddressObj));
-       if (n < 0) error("ERROR writing to socket");
-
+  n = write(sock, &adrObjIn, sizeof(AddressObj));
+       
+  if (n < 0) { 
+    char msg_write[] = "ERROR writing to socket";
+    error(msg_write);
+  }
 }
 
 void fireman(int signum) {
-	while (waitpid(-1, NULL, WNOHANG) > 0)
-		std::cout << "A child process ended" << std::endl;
+  while (waitpid(-1, NULL, WNOHANG) > 0)
+    std::cout << "A child process ended" << std::endl;
 }
 
-int main(int argc, char *argv[])
-{
-     int sockfd, newsockfd, portno, pid;
-     socklen_t clilen;
-     struct sockaddr_in serv_addr, cli_addr;
-     if (argc < 2) {
-         fprintf(stderr,"ERROR, no port provided\n");
-         exit(1);
-     }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-        error("ERROR opening socket");
-     bzero((char *) &serv_addr, sizeof(serv_addr));
-     portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
-     if (bind(sockfd, (struct sockaddr *) &serv_addr,
-              sizeof(serv_addr)) < 0) 
-              error("ERROR on binding");
-     listen(sockfd, 32);
-     clilen = sizeof(cli_addr);
+int main(int argc, char *argv[]) {
+  int sockfd, newsockfd, portno, pid;
+  socklen_t clilen;
+  struct sockaddr_in serv_addr, cli_addr;
      
-     signal(SIGCHLD, fireman);
-     while (true) {
+  if (argc < 2) {
+    fprintf(stderr,"ERROR, no port provided\n");
+    exit(1);    
+  }
+     
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     
+  if (sockfd < 0) { 
+    char msg_open[] = "ERROR opening socket";
+    error(msg_open);
+  }
+     
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);     
+  serv_addr.sin_family = AF_INET;     
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+     
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    char msg_bind[] = "ERROR on binding"; 
+    error(msg_bind);
+  }
 
-       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-       if (newsockfd < 0) 
-          error("ERROR on accept");
-       pid = fork();
-       if (pid < 0)
-	       error("ERROR on fork");
-       if (pid == 0) {
-         close(sockfd);
-         processConnection(newsockfd);
-	 exit(0);
-       } else {
-         close(newsockfd);
-       }
-     }
-     close(sockfd);
-     return 0;
+  listen(sockfd, 32);
+  clilen = sizeof(cli_addr);
+     
+  signal(SIGCHLD, fireman);
+  while (true) {
+
+    newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+       
+    if (newsockfd < 0) { 
+      char msg_accept[] = "ERROR on accept";
+      error(msg_accept);
+    }
+       
+    pid = fork();
+       
+    if (pid < 0) {
+      char msg_fork[] = "ERROR on fork";
+      error(msg_fork);
+    }
+       
+    if (pid == 0) {
+      close(sockfd);
+      processConnection(newsockfd);
+      exit(0);
+    } else {
+      close(newsockfd);
+    }
+  }
+  close(sockfd);
+  return 0;
 }
 
 
